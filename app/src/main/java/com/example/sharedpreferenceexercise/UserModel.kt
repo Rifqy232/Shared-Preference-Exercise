bdfdcb5e43ff1295d40db1403738e9d131f7edb8
Rifqy232 <rachmanmry20d@student.unhas.ac.id>
2023-03-29 00:29:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferenceexercise/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.sharedpreferenceexercise
+
+data class UserModel(
+    var name: String? = null,
+    var email: String? = null,
+    var age: Int? = null,
+    var phoneNumber: String? = null,
+    var isLoved: Boolean = false
+)
